a87fba09e03963489bc9865aef24784f79156e89
Jacek Jabłoski <jacek.jablonski@gmail.com>
2022-07-22 12:07:56 +0200

    feat: initial commit

---
+++ actions/src/merge-job-configs-action.ts
@@ -0,0 +1,20 @@
+import * as core from "@actions/core";
+
+import { mergeJobConfigs } from "./merge-job-configs";
+
+async function run() {
+  const jobConfigsGlobPath = core.getInput("job-configs-glob-path", { required: true });
+  const targetChartPath = core.getInput("target-chart-path", { required: true });
+  await mergeJobConfigs(jobConfigsGlobPath, targetChartPath);
+}
+
+async function runWrapper() {
+  try {
+    await run();
+  } catch (error) {
+    core.setFailed(`merge-job-configs action failed: ${error}`);
+    console.log(error); // eslint-disable-line no-console
+  }
+}
+
+void runWrapper();
